c5d58e895b6e3090c637f17c48d5da9fcf6d986e
karthikreddybethi <58573578+karthikreddybethi@users.noreply.github.com>
2020-03-30 14:39:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -96,16 +96,262 @@ let countryName1 = document.querySelector('.search');
 countryName1.addEventListener('submit',submitData);
 
 function submitData(e) {
-	let data3 = document.querySelector('.country-name').value;
-	console.log(typeof data3);
+	let countries = [
+		"Afghanistan",
+		"Albania",
+		"Algeria",
+		"American Samoa",
+		"Andorra",
+		"Angola",
+		"Anguilla",
+		"Antarctica",
+		"Antigua and Barbuda",
+		"Argentina",
+		"Armenia",
+		"Aruba",
+		"Australia",
+		"Austria",
+		"Azerbaijan",
+		"Bahamas",
+		"Bahrain",
+		"Bangladesh",
+		"Barbados",
+		"Belarus",
+		"Belgium",
+		"Belize",
+		"Benin",
+		"Bermuda",
+		"Bhutan",
+		"Bolivia ",
+		"Bonaire", 
+		"Bosnia and Herzegovina",
+		"Botswana",
+		"Bouvet Island",
+		"Brazil",
+		"Bulgaria",
+		"Burkina Faso",
+		"Burundi",
+		"Cabo Verde",
+		"Cambodia",
+		"Cameroon",
+		"Canada",
+		"Cayman Islands",
+		"Central African Republic",
+		"Chad",
+		"Chile",
+		"China",
+		"Christmas Island",
+		"Colombia",
+		"Congo",
+		"Costa Rica",
+		"Croatia",
+		"Cuba",
+		"Curaçao",
+		"Cyprus",
+		"Czechia",
+		"Denmark",
+		"Djibouti",
+		"Dominica",
+		"Dominican Republic",
+		"Ecuador",
+		"Egypt",
+		"El Salvador",
+		"Equatorial Guinea",
+		"Eritrea",
+		"Estonia",
+		"Eswatini",
+		"Ethiopia",
+		"Fiji",
+		"Finland",
+		"France",
+		"French Guiana",
+		"French Polynesia",
+		"Gabon",
+		"Gambia",
+		"Georgia",
+		"Germany",
+		"Ghana",
+		"Gibraltar",
+		"Greece",
+		"Greenland",
+		"Grenada",
+		"Guadeloupe",
+		"Guam",
+		"Guatemala",
+		"Guernsey",
+		"Guinea",
+		"Guinea-Bissau",
+		"Guyana",
+		"Haiti",
+		"Heard Island and McDonald Islands",
+		"Honduras",
+		"Hong Kong",
+		"Hungary",
+		"Iceland",
+		"India",
+		"Indonesia",
+		"Iran",
+		"Iraq",
+		"Ireland",
+		"Isle of Man",
+		"Israel",
+		"Italy",
+		"Jamaica",
+		"Japan",
+		"Jersey",
+		"Jordan",
+		"Kazakhstan",
+		"Kenya",
+		"Kiribati",
+		"Kuwait",
+		"Kyrgyzstan",
+		"Latvia",
+		"Lebanon",
+		"Lesotho",
+		"Liberia",
+		"Libya",
+		"Liechtenstein",
+		"Lithuania",
+		"Luxembourg",
+		"Macao",
+		"Madagascar",
+		"Malawi",
+		"Malaysia",
+		"Maldives",
+		"Mali",
+		"Malta",
+		"Martinique",
+		"Mauritania",
+		"Mauritius",
+		"Mayotte",
+		"Mexico",
+		"Micronesia",
+		"Moldova",
+		"Monaco",
+		"Mongolia",
+		"Montenegro",
+		"Montserrat",
+		"Morocco",
+		"Mozambique",
+		"Myanmar",
+		"Namibia",
+		"Nauru",
+		"Nepal",
+		"Netherlands",
+		"New Caledonia",
+		"New Zealand",
+		"Nicaragua",
+		"Niger",
+		"Nigeria",
+		"Niue",
+		"Norfolk Island",
+		"Norway",
+		"Oman",
+		"Pakistan",
+		"Palau",
+		"Palestine",
+		"Panama",
+		"Papua New Guinea",
+		"Paraguay",
+		"Peru",
+		"Philippines",
+		"Pitcairn",
+		"Poland",
+		"Portugal",
+		"Puerto Rico",
+		"Qatar",
+		"Republic of North Macedonia",
+		"Romania",
+		"Russia",
+		"Rwanda",
+		"Réunion",
+		"Saint Kitts and Nevis",
+		"Saint Lucia",
+		"Saint Martin",
+		"San Marino",
+		"Saudi Arabia",
+		"Senegal",
+		"Serbia",
+		"Seychelles",
+		"Sierra Leone",
+		"Singapore",
+		"Sint Maarten",
+		"Slovakia",
+		"Slovenia",
+		"Solomon Islands",
+		"Somalia",
+		"South Africa",
+		"Sudan",
+		"Spain",
+		"Sri Lanka",
+		"Suriname",
+		"Sweden",
+		"Switzerland",
+		"Syria",
+		"Taiwan",
+		"Tajikistan",
+		"Tanzania",
+		"Thailand",
+		"Timor-Leste",
+		"Togo",
+		"Tokelau",
+		"Tonga",
+		"Trinidad and Tobago",
+		"Tunisia",
+		"Turkey",
+		"Turkmenistan",
+		"Tuvalu",
+		"Uganda",
+		"Ukraine",
+		"uae",
+		"uk",
+		"usa",
+		"Uruguay",
+		"Uzbekistan",
+		"Vanuatu",
+		"Venezuela",
+		"VietNam",
+		"Virgin Islands",
+		"Yemen",
+		"Zambia",
+		"Zimbabwe"
+	];
+
+	let data3 = document.querySelector('.country-name').value.toLowerCase();
+	let data4 = "";
+	const newCountries = countries.map((country) => {
+		return country.toLowerCase()
+	})
 	
-	document.querySelector('.main-heading').innerText = `${data3} Stats` ;
-	document.querySelector('.main-heading').style.textTransform = "capitalize";
-	let dataCovid = new Covid();
-	if (data3 ===''){
-		dataCovid.get().then(data);
-	}else {
-		dataCovid.get(data3).then(data);
+	newCountries.forEach((country) => {
+		if(country === data3) {
+			data4 = data3;
+			document.querySelector('.main-heading').innerText = `${data3} Stats` ;
+			
+			document.querySelector('.main-heading').style.textTransform = "capitalize";
+			document.querySelector('.main-heading').style.transition = `all 2000ms ease 0s` ;
+			document.querySelector('.warning-message').style.display = 'none';
+
+			let dataCovid = new Covid();
+			if (data3 ===''){
+				dataCovid.get().then(data);
+			} else if( data3 === 'poland'){
+				dataCovid.get(data3).then(data);
+				document.querySelector(".silesian-vivodship").style.display = 'block';
+			}
+			else {
+				dataCovid.get(data3).then(data);
+				document.querySelector(".silesian-vivodship").style.display = 'none';
+			}
+
+		}
+
+	})
+
+	if (data4 !== data3){
+		document.querySelector('.main-heading').innerHTML = `Please enter a valid country name <br> <span class="warning-message">(In case of the USA and the UK search as USA and UK)</span> ` ;
+		// document.querySelector('.warning-message').textContent = `In case of the USA and the UK search as USA and UK`;
+		
 	}
 	e.preventDefault();
 }
